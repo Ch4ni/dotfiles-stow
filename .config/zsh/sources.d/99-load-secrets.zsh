4fd0b66ff017225636319a3633753c0293feb902
Ch4ni <Ch4ni@adahome.dev>
2025-12-11 17:54:22 -0800

    add secrets dir creation, and comments ...
    ... to the secrets loader since this is an inherently insecure
    method of loading secrets

--- .config/zsh/sources.d/99-load-secrets.zsh
+++ .config/zsh/sources.d/99-load-secrets.zsh
@@ -1,5 +1,21 @@
 #! /usr/bin/env zsh
 
+# This is kind of a hack-job meant to unblock me until I've got time to set up a proper
+# shell secrets management solution.
+#
+# Define the default secrets dir (~/.config/secrets), and if it doesn't exist then
+# create it with rwx permissions for only the owner.
+# Iterate through all of the files in the directory, and source them.
+
+# TODO: replace this with an actual dynamic secrets management solution. Preferably
+# something that's encrypted at rest. In the meantime:
+# DO NOT COMMIT THE CONTENTS OF ~/.config/secrets TO SOURCE CONTROL.
+
+SECRETS_D_DIR=${SECRETS_D_DIR:-${XDG_CONFIG_HOME:-${HOME}/.config}/secrets}
+if [[ ! -d ${SECRETS_D_DIR} ]]; then
+  install -d -m 0700 ${SECRETS_D_DIR}
+fi
+
 for secretfile in ${HOME}/.config/secrets/*.zsh; do
     source ${secretfile}
 done
