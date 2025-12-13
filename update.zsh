8469141c995ab1a84321e76404e4467bcc5bc50d
Ch4ni <Ch4ni@adahome.dev>
2025-12-13 13:37:48 -0800

    refactor update script, remove remove script
    update script:
    - process packages in order under packages/
    - auto-detect new packages under packages/
    - auto-detect config packages under packages/ by '-cfg' suffix
      (installed under ~/.config)
    - support for increased verbosity on update.zsh
    - support for un-stowing on update.zsh

--- update.zsh
+++ update.zsh
@@ -1,14 +1,26 @@
 #! /usr/bin/env zsh
 
-set -x
+zparseopts -D -K -- \
+	{d,-unstow}=unstow \
+	{v+,-verbose+}=verbose
+
+
 config_dir=${XDG_CONFIG_HOME:-${HOME}/.config}
 repo_dir=${0:h}
 pkg_dir=${repo_dir}/packages
-set +x
 
-stow -vv -d ${pkg_dir} -t ${config_dir} --dotfiles 10-mise-cfg
-stow -vv -d ${pkg_dir} -t ${config_dir} --dotfiles 19-starship-cfg
-stow -vv -d ${pkg_dir} -t ${HOME} --dotfiles 20-zsh
-stow -vv -d ${pkg_dir} -t ${HOME} --dotfiles 40-git
-stow -vv -d ${pkg_dir} -t ${HOME} --dotfiles 50-nvim
+for package in ${pkg_dir}/[0-9][0-9]-*(/); do
+    pkg_name=${package:t}
+
+    if [[ ${pkg_name} == *-cfg ]]; then
+	printf '\n=== installing configuration package: %s\n\n' ${${pkg_name#??-}%-cfg}
+	stow --verbose=${#verbose} -d ${pkg_dir} -t ${config_dir} -R --dotfiles ${unstow:+-D} ${pkg_name} 2>&1 | sed 's/^/\t/'
+
+    else
+	printf '\n=== installing package: %s\n\n' ${${pkg_name#??-}%-cfg}
+	stow --verbose=${#verbose} -d ${pkg_dir} -t ${HOME} -R --ignore '.*/.steam/.*' --dotfiles  ${unstow:+-D} ${pkg_name} 2>&1 | sed -e '/^BUG in find_stowed_path/d' -e 's/^/\t/'
+
+    fi
+
+done
 
