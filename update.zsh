af4cc7fcfc7fcd59c85baeff6a024a5d46a81b4a
Ch4ni <Ch4ni@adahome.dev>
2025-12-13 10:09:34 -0800

    refactor - move mise into its own stow package

--- update.zsh
+++ update.zsh
@@ -1,9 +1,10 @@
 #! /usr/bin/env zsh
 
-stow -vv -t ${HOME} initial
+stow -vv -t ${HOME} --dotfiles initial
+stow -vv -t ${HOME} --dotfiles mise
 stow -vv -t ${HOME} --dotfiles zsh
 stow -vv -t ${HOME} --dotfiles git
-stow -vv -t ${HOME} nvim
+stow -vv -t ${HOME} --dotfiles nvim
 
 # TODO: refactor zsh into its own package
 # TODO: refactor git into its own package
